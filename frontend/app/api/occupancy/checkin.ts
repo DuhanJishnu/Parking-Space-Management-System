09363fdcdbcbb98fad527a78d6da2e7694fa7cc8
Jishnu Duhan <duhanjishnu@gmail.com>
2025-11-18 10:46:31 +0530

    Revert "front end : 9:53 am"
    This reverts commit 91fa888f4d1847f9420d2f3c3ff4cd097b335614.

--- frontend/app/api/occupancy/checkin.ts
+++ frontend/app/api/occupancy/checkin.ts
@@ -4,13 +4,8 @@ export const checkIn = async (payload: {
   user_id: number;
   vehicle_id: number;
   space_id: number;
-  vehicle_type: string;
 }) => {
-  console.log("payload in checkin : ", payload);
-  const vehicleDetails = await API.post("/vehicle/", {vehicle_id: payload.vehicle_id, vehicle_type:payload.vehicle_type});
-  console.log("vehicle details in checkin.ts", vehicleDetails.data);
-  
-  const res = await API.post("/occupancy/check-in", payload: newPayload);
+  const res = await API.post("/occupancy/check-in", payload);
   console.log("Check-in log : , res.data");
   return res.data;
 };
